dc392e3d5ab45a18161fd5a00a5e5a8578114f6d
Eder Canada <edercanada@Eders-MacBook-Pro.local>
2019-10-07 15:43:47 -0700

    code

--- js/app.js
+++ js/app.js
@@ -87,11 +87,11 @@ const game = {
 
 
 
-
-
-
 const button = document.querySelectorAll('.button')
 for(let i = 0; i < button.length; i++) {
 button[i].addEventListener('click', game.locateChip)
 
 }
+
+
+const color = 'red'
