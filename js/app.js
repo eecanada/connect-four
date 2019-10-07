58e7a6edea760f7af4604134dd0634a91c53244c
Eder Canada <edercanada@Eders-MacBook-Pro.local>
2019-10-07 15:20:56 -0700

    create board

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
+//name
+// $('#begin').on('click',()=>{
+// const setName = prompt('Name your player 1');
+// game.player1 = setName 
+// const name1 = new Player (game.player1)
+// $one = $('#player1')
+// $one.text(`Player 1: ${name1.name}`)
 
-$('#begin').on('click',()=>{
-const setName = prompt('Name your player 1');
-game.player1 = setName 
-const name1 = new Player (game.player1)
-$one = $('#player1')
-$one.text(`Player 1: ${name1.name}`)
-
-});
+// });
 
 
 
-$('#begin').on('click',()=>{
-  const setName2 = prompt('Name your player 2');
-  game.player2 = setName2 
-  const name2 = new Player (game.player2)
-  $two = $('#player2')
-  $two.text(`Player 2: ${name2.name}`)
-});
+// $('#begin').on('click',()=>{
+//   const setName2 = prompt('Name your player 2');
+//   game.player2 = setName2 
+//   const name2 = new Player (game.player2)
+//   $two = $('#player2')
+//   $two.text(`Player 2: ${name2.name}`)
+// });
   
 
 
@@ -34,20 +34,48 @@ $('#begin').on('click',()=>{
 
 // game object 
 const game = {
-player1: '',
-player2: '',
-score1: 0,
-score2: 0,
+  board: [
+    ['', '', '', '', '', '', '',],
+    ['', '', '', '', '', '', '',],
+    ['', '', '', '', '', '', '',],
+    ['', '', '', '', '', '', '',],
+    ['', '', '', '', '', '', '',],
+    ['', '', '', '', '', '', '',]    
+  ],
+
+  //name
+  // player1: '',
+  // player2: '',
+  score1: 0,
+  score2: 0,
+  playerOne: true,
 
 
-player1Choice(){
-
-},
+  createBoard(){
 
 
-player2choice(){
+  },
 
-},
+
+  player1Move(){
+
+  },
+
+
+  player2Move(){
+
+  },
+
+
+  checkWinner(){
+
+  },
+
+
+
+
+
+
 
 
 }
