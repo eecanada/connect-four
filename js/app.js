89826da06ff1ba89e04070bc885413a33223ab3a
Eder Canada <edercanada@Eders-MacBook-Pro.local>
2019-10-07 15:35:15 -0700

    add player name

--- js/app.js
+++ js/app.js
@@ -1,27 +1,22 @@
-//name
-// $('#begin').on('click',()=>{
-// const setName = prompt('Name your player 1');
-// game.player1 = setName 
-// const name1 = new Player (game.player1)
-// $one = $('#player1')
-// $one.text(`Player 1: ${name1.name}`)
-
-// });
-
-
-
-// $('#begin').on('click',()=>{
-//   const setName2 = prompt('Name your player 2');
-//   game.player2 = setName2 
-//   const name2 = new Player (game.player2)
-//   $two = $('#player2')
-//   $two.text(`Player 2: ${name2.name}`)
-// });
+  $('#begin').on('click',()=>{
+  const setName = prompt('Name your player 1');
+  game.player1 = setName 
+  const name1 = new Player (game.player1)
+  $one = $('#player1')
+  $one.text(`Player 1: ${name1.name}`)
   
-
-
+  });
   
   
+  
+  $('#begin').on('click',()=>{
+    const setName2 = prompt('Name your player 2');
+    game.player2 = setName2 
+    const name2 = new Player (game.player2)
+    $two = $('#player2')
+    $two.text(`Player 2: ${name2.name}`)
+  });
+    
   
   class Player {
     constructor(name){ // what the user decides
@@ -44,8 +39,8 @@ const game = {
   ],
 
   //name
-  // player1: '',
-  // player2: '',
+  player1: '',
+  player2: '',
   score1: 0,
   score2: 0,
   playerOne: true,
