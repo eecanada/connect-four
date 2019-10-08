b6dd68698aa22808868206c9873ef726e2a7fd70
Eder Canada <canada.eder@gmail.com>
2019-10-07 20:01:23 -0700

    board and array fill

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+  
+  // allows prompt to show up and give player 1/2 a name
   $('#begin').on('click',()=>{
   const setName = prompt('Name your player 1');
   game.player1 = setName 
@@ -41,59 +43,81 @@ const game = {
   //name
   player1: '',
   player2: '',
-  score1: 0,
-  score2: 0,
+  score: 0,
   playerOne: true,
 
-
-  createBoard(){
-
-
-  },
-
-
-  player1Move(){
-
-  },
-
-
-  player2Move(){
-
-  },
-
-
   checkWinner(){
+    // if (game.board === )
 
   },
 
 
-
+// this allows me let player 1/2 put chip on the board and locate where they are on the board 
   locateChip: (e) => {
-    if(this.playerOne){
+    if(game.playerOne){
+      // console.log(e.target)
+      // e.target.style.backgroundColor = "red"
       console.log(e.target.cellIndex, "col")
       console.log(e.target.parentElement.rowIndex, "row")
-      e.target.style.backgroundColor = "red"
-      this.playerOne = false
+      // game.board.push(e.target) // this would push the value into the array
+
+
+      // Checks the column from bottom to top
+      for(let i = game.board.length - 1; i >= 0; i--){
+        // checks if lowest row/column has a value
+        // if it does dont fill that one and move on
+        
+        // if the row/column is empty then fill it
+        if(!game.board[i][e.target.cellIndex]){
+          game.board[i][e.target.cellIndex] = 1;
+          break;
+        }
+      }
+
+      // game.board[e.target.parentElement.rowIndex][e.target.cellIndex] = 1;
+      game.playerOne = false
     } else {
-      console.log(e.target)
-      e.target.style.backgroundColor = "yellow"
+      // console.log(e.target)
+      // e.target.style.backgroundColor = "yellow"
       console.log(e.target.cellIndex, "col")
       console.log(e.target.parentElement.rowIndex, "row")
-      this.playerOne = true
+
+
+      for(let i = game.board.length - 1; i >= 0; i--){
+        if(!game.board[i][e.target.cellIndex]){
+          game.board[i][e.target.cellIndex] = 2;
+          break;
+        }
+      }
+
+      // game.board[e.target.parentElement.rowIndex][e.target.cellIndex] = 2;
+      // game.board.push(e.target)
+      game.playerOne = true
+    }
+    game.loadBoard();
+  },
+
+  loadBoard: () => {
+
+    
+    // const rows = document.querySelectorAll('tr');
+    for (let i = 0; i < game.board.length; i++) {
+      for (let o = 0; o < game.board[i].length; o++){
+        if (game.board[i][o] === 1 ){
+          rows[i].children[o].style.backgroundColor = "red"
+        } else if (game.board[i][o] === 2){
+          rows[i].children[o].style.backgroundColor = "yellow"
+        }
+      }
+      
     }
   }
 
 };
 
-
-
+const rows = document.querySelectorAll('tr');
+// 
 const button = document.querySelectorAll('.button')
 for(let i = 0; i < button.length; i++) {
 button[i].addEventListener('click', game.locateChip)
-
 }
-
-
-const color = 'red'
-
-const mike = 'thank you!'
